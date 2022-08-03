48b0c8c8d6a21400b0dfb3a57603e21a4eaf35b6
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-08-03 20:54:37 +0100

    First GUI design - not functional

--- GUI/vid2photogrammetryGUI.py
+++ GUI/vid2photogrammetryGUI.py
@@ -1,14 +1,20 @@
-
 from PyQt5.QtWidgets import *
+from PyQt5 import uic
 
 import vid2photogrammetry
 
+class Gui(QMainWindow):
+    def __init__(self):
+        super(Gui, self).__init__()
+        uic.loadUi('testGui.ui', self)
+        self.show()
+
 def main():
     app = QApplication([])
-    window = QWidget()
-    window.setWindowTitle("Vid2Photogrammetry")
-    window.show()
+    window = Gui()
     app.exec_()
+
+
 
 if __name__ == '__main__':
     main()
