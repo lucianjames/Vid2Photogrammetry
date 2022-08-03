768a9ae4b7a58ae0de5500e20e01ce0be19bd253
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-08-03 22:09:34 +0100

    Create new build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir libVid2Photogrammetry/build/
+cd libVid2Photogrammetry/build/
+cmake ..
+cmake --build .
+cp vid2photogrammetry.cpython-* ../../GUI/
+cd ../../
+mkdir pybuild
+cd pybuild
+python3 -m PyInstaller ../GUI/vid2photogrammetryGUI.py
+cp ../GUI/*.ui dist/vid2photogrammetryGUI/
--- buildPythonModule.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir libVid2Photogrammetry/build/
-cmake libVid2Photogrammetry/
-cmake --build libVid2Photogrammetry/build
-cp libVid2Photogrammetry/build/vid2photogrammetry.cpython-* GUI/
