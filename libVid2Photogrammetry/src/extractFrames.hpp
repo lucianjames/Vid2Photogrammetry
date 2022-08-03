cf3bc0451fae786d48d3821fcc482983204e6eb1
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-08-03 17:12:50 +0100

    Move files around
    Created new extractFrames.hpp/cpp files
    Deleted vid2photogrammetry.cpp because vid2photogrammetry.hpp will just include other header files from now on.

---
+++ libVid2Photogrammetry/src/extractFrames.hpp
@@ -0,0 +1,3 @@
+#include <string>
+
+void extractFrames(std::string inputPath, std::string outputPath, std::string outputName, std::string outputExtension, int frameCount);
--- libVid2Photogrammetry/src/vid2photogrammetry.hpp
+++ libVid2Photogrammetry/src/vid2photogrammetry.hpp
@@ -1,3 +1,1 @@
-#include <string>
-
-void extractFrames(std::string inputPath, std::string outputPath, std::string outputName, std::string outputExtension, int frameCount);
+#include "extractFrames.hpp"
