00e4142f1ab17a1138fc2872533b2c9d7366124a
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-08-04 13:47:01 +0100

    Revert "add something to look for DLL files"
    This reverts commit 415d05c686bfce403971d6355dee9eb110d0c220.

--- pyinstaller.sh
+++ pyinstaller.sh
@@ -3,5 +3,4 @@ mkdir pybuild
 cd pybuild
 mkdir dist
 cp ../GUI/*.ui .
-cp ../DLLS/*.dll . # So shit works on windows
-python3 -m PyInstaller --onefile --add-data '*.ui:.' --add-data '*.dll:.' ../GUI/vid2photogrammetryGUI.py --noconfirm
+python3 -m PyInstaller --onefile --add-data '*.ui:.' ../GUI/vid2photogrammetryGUI.py --noconfirm
