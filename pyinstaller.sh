7e042258116110cbb41878e21f3ab29e4037c0ff
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-08-04 13:07:38 +0100

    add data for ui files not working as i hoped

--- pyinstaller.sh
+++ pyinstaller.sh
@@ -2,5 +2,5 @@
 mkdir pybuild
 cd pybuild
 mkdir dist
-cp ../GUI/*.ui .
-python3 -m PyInstaller --onefile --add-data '*.ui:.' ../GUI/vid2photogrammetryGUI.py --noconfirm
+cp ../GUI/*.ui dist/
+python3 -m PyInstaller --onefile ../GUI/vid2photogrammetryGUI.py --noconfirm
