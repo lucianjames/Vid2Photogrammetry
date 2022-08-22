c1ee64a31b355aa5c3d6757f9856c787f714d5e1
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-08-22 11:18:23 +0100

    Update buildWindows.sh

--- buildWindows.sh
+++ buildWindows.sh
@@ -3,4 +3,4 @@ mkdir libVid2Photogrammetry/build/
 cd libVid2Photogrammetry/build/
 cmake ..
 cmake --build . --config Release
-cp vid2photogrammetry.cp* ../../GUI/
+cp Release/vid2photogrammetry.cp* ../../GUI/
