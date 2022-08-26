1dbc56e5912466a2b6cc8880f0b0b8aff78fdb45
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-08-26 18:27:06 +0100

    Trying to use unordered map to limit comparisons, not working....

--- libVid2Photogrammetry/src/deleteNearDuplicates.cpp
+++ libVid2Photogrammetry/src/deleteNearDuplicates.cpp
@@ -3,6 +3,8 @@
 
 #include <string>
 #include <filesystem>
+#include <vector>
+#include <unordered_map>
 
 cv::Mat calcHistNorm(cv::Mat input){
     cv::Mat hist;
@@ -19,7 +21,15 @@ cv::Mat calcHistNorm(cv::Mat input){
     return hist;
 }
 
-void deleteNearDuplicates(std::string framesFolder){
+void deleteNearDuplicates(std::string framesFolder, float threshold){
+    std::cout << "==================================================" << std::endl;
+    std::cout << "Removing (near) duplicate frames...." << std::endl;
+    std::cout << "Performing validation..." << std::endl;
+    // Check framesFolder exists
+    if(!std::filesystem::exists(framesFolder)){
+        std::cout << "ERR: folder does not exist" << std::endl;
+        return;
+    }
     // Get the list of files in the framesFolder
     std::vector<std::string> files;
     for(auto& p : std::filesystem::directory_iterator(framesFolder)){
@@ -28,14 +38,31 @@ void deleteNearDuplicates(std::string framesFolder){
     
     // This is disgustingly O(n^2).
     // Could be optimised a bit though.
+    std::cout << "Performing histogram analysis and frame removal..." << std::endl;
+    std::vector<std::string> toDelete;
+    std::unordered_map<std::string, bool> alreadyCompared;
     for(auto fp : files){
         cv::Mat img1 = cv::imread(fp);
         cv::Mat img1_hist = calcHistNorm(img1);
+        alreadyCompared[fp+fp] = true;
         for(auto fp2 : files){
-            cv::Mat img2 = cv::imread(fp2);
-            cv::Mat img2_hist = calcHistNorm(img2);
-            std::cout << fp << " | " << fp2 << std::endl;
-            std::cout << cv::compareHist(img1_hist, img2_hist, cv::HISTCMP_BHATTACHARYYA) << std::endl;
+            std::cout << alreadyCompared[fp+fp2] << std::endl;
+            if(alreadyCompared[fp+fp2] || alreadyCompared[fp2+fp]){
+                cv::Mat img2 = cv::imread(fp2);
+                cv::Mat img2_hist = calcHistNorm(img2);
+                std::cout << fp << " | " << fp2 << std::endl;
+                float unSimilarity = cv::compareHist(img1_hist, img2_hist, cv::HISTCMP_BHATTACHARYYA);
+                std::cout << unSimilarity << std::endl;
+                if(unSimilarity > threshold){
+                    toDelete.push_back(fp2);
+                    std::cout << fp << " and " << fp2 << " ARE TOO SIMILAR APPARENTLY!LRYTUYT!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+                }
+            }
         }
     }
+    int removed = toDelete.size();
+    for(auto fp : toDelete){
+        std::filesystem::remove(fp);
+    }
+    std::cout << "Duplicate frame removal complete (" << removed << " deleted)" << std::endl;
 }
--- libVid2Photogrammetry/src/deleteNearDuplicates.hpp
+++ libVid2Photogrammetry/src/deleteNearDuplicates.hpp
@@ -3,4 +3,4 @@
 #include <string>
 
 // This is going to be O(n^2) :(
-void deleteNearDuplicates(std::string framesFolder);
+void deleteNearDuplicates(std::string framesFolder, float threshold);
