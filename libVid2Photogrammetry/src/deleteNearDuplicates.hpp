17a7d6a12d97c15da0e553096ee63fbe5adbe5fb
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-08-31 12:11:25 +0100

    Move histogram comparison stuff into commonFunctions.hpp as they are going to be used in deleteOutliers.hpp
    (as well as deleteNearDuplicates.hpp)

--- libVid2Photogrammetry/src/deleteNearDuplicates.hpp
+++ libVid2Photogrammetry/src/deleteNearDuplicates.hpp
@@ -8,19 +8,26 @@
 
 #include "commonFunctions.hpp"
 
-cv::Mat calcHistNorm(cv::Mat input){
-    cv::Mat hist;
-    // Convert to HSV
-    cv::cvtColor(input, input, cv::COLOR_BGR2HSV);
-    int h_bins = 50, s_bins = 60;
-    int histSize[] = {h_bins, s_bins};
-    float h_ranges[] = {0, 180};
-    float s_ranges[] = {0, 256};
-    const float* ranges[] = {h_ranges, s_ranges};
-    int channels[] = {0, 1};
-    cv::calcHist(&input, 1, channels, cv::Mat(), hist, 2, histSize, ranges, true, false);
-    cv::normalize(hist, hist, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
-    return hist;
+// Takes a folder path and threshold as input, and returns a vector of strings containing the paths of images which are similar enough to at least 1 other image in the folder
+std::vector<std::string> getSimilarImagePaths(std::string framesFolder, float threshold){
+    std::cout << "Using HISTCMP_BHATTACHARYYA" << std::endl;
+    std::vector<std::string> files = getFilepathsInFolder(framesFolder);
+    std::vector<std::string> similarFrames; // Stores the frames which are similar enough to at least one other frame to be kept
+    std::unordered_map<std::string, bool> alreadyCompared; // Used to avoid doing the same comparison (or the same comparison the other way around) multiple times.
+    for(auto fp : files){
+        cv::Mat fp1hist = calcHistNorm(cv::imread(fp));
+        alreadyCompared[fp+fp] = true;
+        for(auto fp2 : files){
+            if(!(alreadyCompared[fp+fp2]) && !(alreadyCompared[fp2+fp])){
+                float unSimilarity = cv::compareHist(fp1hist, calcHistNorm(cv::imread(fp2)), cv::HISTCMP_BHATTACHARYYA);
+                if((unSimilarity < threshold) && (std::find(similarFrames.begin(), similarFrames.end(), fp2) == similarFrames.end())){
+                    similarFrames.push_back(fp2);
+                }
+                alreadyCompared[fp+fp2] = true;
+            }
+        }
+    }
+    return similarFrames;
 }
 
 void deleteNearDuplicates(std::string framesFolder, float threshold){
@@ -35,22 +42,7 @@ void deleteNearDuplicates(std::string framesFolder, float threshold){
     
     // Basically O(n^2), but ive optimised it a little bit
     std::cout << "Performing histogram analysis and frame removal..." << std::endl;
-    std::cout << "Using HISTCMP_BHATTACHARYYA" << std::endl;
-    std::vector<std::string> toDelete; // Delete after loop to avoid having to account for fucky stuff.
-    std::unordered_map<std::string, bool> alreadyCompared; // Used to avoid doing the same comparison (or the same comparison the other way around) multiple times.
-    for(auto fp : files){
-        cv::Mat fp1hist = calcHistNorm(cv::imread(fp));
-        alreadyCompared[fp+fp] = true;
-        for(auto fp2 : files){
-            if(!(alreadyCompared[fp+fp2]) && !(alreadyCompared[fp2+fp])){
-                float unSimilarity = cv::compareHist(fp1hist, calcHistNorm(cv::imread(fp2)), cv::HISTCMP_BHATTACHARYYA);
-                if((unSimilarity < threshold) && (std::find(toDelete.begin(), toDelete.end(), fp2) == toDelete.end())){
-                    toDelete.push_back(fp2);
-                }
-                alreadyCompared[fp+fp2] = true;
-            }
-        }
-    }
+    std::vector<std::string> toDelete = getSimilarImagePaths(framesFolder, threshold);
     int removed = toDelete.size();
     for(auto fp : toDelete){
         std::filesystem::remove(fp);
