109a297268631228cb150f0b529002cd7a8e311a
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-09-15 13:59:21 +0100

    Edit palette a bit

--- main.cpp
+++ main.cpp
@@ -5,18 +5,27 @@
 
 QPalette createTheme(){ // Returns a nice looking theme for the application
     QPalette palette;
-    palette.setColor(QPalette::Window, QColor(53,53,53));
+    QColor darkGray(53, 53, 53);
+    QColor gray(128, 128, 128);
+    QColor black(25, 25, 25);
+    QColor blue(42, 130, 218);
+    palette.setColor(QPalette::Window, darkGray);
     palette.setColor(QPalette::WindowText, Qt::white);
-    palette.setColor(QPalette::Base, QColor(15,15,15));
-    palette.setColor(QPalette::AlternateBase, QColor(53,53,53));
-    palette.setColor(QPalette::ToolTipBase, Qt::white);
+    palette.setColor(QPalette::Base, black);
+    palette.setColor(QPalette::AlternateBase, darkGray);
+    palette.setColor(QPalette::ToolTipBase, blue);
     palette.setColor(QPalette::ToolTipText, Qt::white);
     palette.setColor(QPalette::Text, Qt::white);
-    palette.setColor(QPalette::Button, QColor(53,53,53));
+    palette.setColor(QPalette::Button, darkGray);
     palette.setColor(QPalette::ButtonText, Qt::white);
-    palette.setColor(QPalette::BrightText, Qt::red);
-    palette.setColor(QPalette::Highlight, QColor(142,45,197).lighter());
+    palette.setColor(QPalette::Link, blue);
+    palette.setColor(QPalette::Highlight, blue);
     palette.setColor(QPalette::HighlightedText, Qt::black);
+    palette.setColor(QPalette::Active, QPalette::Button, gray.darker());
+    palette.setColor(QPalette::Disabled, QPalette::ButtonText, gray);
+    palette.setColor(QPalette::Disabled, QPalette::WindowText, gray);
+    palette.setColor(QPalette::Disabled, QPalette::Text, gray);
+    palette.setColor(QPalette::Disabled, QPalette::Light, darkGray);
     return palette;
 }
 
